522c000559b2401e0b5bd31bdfc1d14209ceaafe
Mandy Schwarze <mndyscode@me.com>
2021-03-09 00:53:00 +0100

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,55 @@
 import styled from 'styled-components'
+import Header from './Header'
 
 function App() {
-  return <Grid>Hello World!</Grid>
+  return (
+    <Grid>
+      <Header>Wheel of TBR</Header>
+      <main>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+        est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
+        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit.
+      </main>
+    </Grid>
+  )
 }
 
 const Grid = styled.div`
   display: grid;
+  justify-content: center;
+  grid-template-columns: 1fr;
+  grid-template-rows: minmax(70px, 6vh) 1fr 6vh;
+  margin: 0 auto;
   height: 100vh;
-  place-content: center;
-  place-items: center;
+  min-width: 320px;
+  max-width: 900px;
+
+  main {
+    padding: 10% 5%;
+    overflow-y: auto;
+  }
 `
 
 export default App
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/macro'
+import afterHeader from '../images/wavesOpacity.svg'
+
+export default function Header({ children }) {
+  return (
+    <HeaderStyled>
+      <Title>{children}</Title>
+    </HeaderStyled>
+  )
+}
+
+const HeaderStyled = styled.header`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-content: top;
+  background: linear-gradient(#0f6f7b 0%, #093a40 100%);
+
+  ::after {
+    content: url(${afterHeader});
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    height: 5em;
+  }
+`
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: min(max(5vw, 40px), 60px);
+  font-family: 'Hanging Letters';
+  font-weight: normal;
+  letter-spacing: 0.15em;
+  color: #e4e4e4;
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.84);
+`
