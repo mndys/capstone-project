bafafaf0b653992465afe1264e64f5bcb13ddabd
Mandy Schwarze <mndyscode@me.com>
2021-03-09 22:57:01 +0100

    Create Button Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
+import { useState } from 'react'
 import styled from 'styled-components/macro'
+import Button from './Button'
 import Header from './Header'
 
 function App() {
+  const prompt = [
+    'big book',
+    'bought recently',
+    'classic',
+    'cover colour',
+    'dark academia',
+    'dnf',
+    'fantasy',
+    'female protagonist',
+    'friend pick',
+    'graphic novel',
+    'haunted setting',
+    'highest rated',
+    'lowest rated',
+    'male protagonist',
+    'un-read author',
+    'new-ish',
+    'non-fiction',
+    'page number',
+    'romance',
+    'short read',
+    'thriller',
+  ]
+
+  const [currentPrompt, setCurrentPrompt] = useState('')
+
   return (
     <Grid>
       <Header>Wheel of TBR</Header>
       <main>
+        <FlexWrapper>
+          <Button primary onClick={getPrompt}>
+            Spin!
+          </Button>
+        </FlexWrapper>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -18,23 +51,22 @@ function App() {
         elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
         aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
         dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-        est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
-        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-        takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-        amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-        invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit.
+        est Lorem ipsum dolor sit amet.
       </main>
     </Grid>
   )
+
+  function getPrompt() {
+    alert('prompt')
+    console.log('prompt')
+  }
 }
+/* function App() {
+  return <Button handleClick={getPrompt}>Click me!</Button>
+  function getPrompt() {
+    alert('Hello!')
+  }
+} */
 
 const Grid = styled.div`
   display: grid;
@@ -50,6 +82,12 @@ const Grid = styled.div`
     padding: 10% 5%;
     overflow-y: auto;
   }
+`
+
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
 `
 
 export default App
---
+++ src/components/Button.spec.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import Button from './Button'
+
+it('renders a Button', () => {
+  render(<Button />)
+  expect(screen.getByRole('button')).toBeVisible()
+})
+
+it('renders a Button with textContent "Spin!"', () => {
+  render(<App />)
+  expect(screen.getByText('Spin!')).toBeVisible()
+})
+
+it('renders a grey gradient Button if Button is disabled', () => {
+  render(<Button disabled>This is a button</Button>)
+  expect(screen.getByRole('button')).toHaveStyle(
+    'background: linear-gradient(#e5e5e5 0%, #c3c3c3 100%)'
+  )
+})
+
+it('renders a green gradient Button if Button has attribute primary', () => {
+  render(<Button primary>This is a button</Button>)
+  expect(screen.getByRole('button')).toHaveStyle(
+    'background: linear-gradient(#0f6f7b 0%, #093a40 100%)'
+  )
+})
+
+it.todo('gets a prompt on click')
