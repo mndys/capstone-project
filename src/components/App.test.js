d23ec36e3172fa69323f432e2e8ba6b426aa27f5
Mandy Schwarze <mndyscode@me.com>
2021-03-10 19:16:15 +0100

    Change test for Button color; move Button test in App scope to App.spec.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,3 +5,8 @@ it('renders the App grid', () => {
   render(<App />)
   expect(screen.getByRole('banner').closest('div')).toHaveStyle('display: grid')
 })
+
+it('renders a Button with textContent "Spin!"', () => {
+  render(<App />)
+  expect(screen.getByRole('button', { name: 'Spin!' })).toBeVisible()
+})
