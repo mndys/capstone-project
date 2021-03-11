9e7ad43946d2bd097698e7cd1f3393530a1c164a
Mandy Schwarze <mandy2301@me.com>
2021-03-11 18:37:12 +0100

    Add testing to-dos

--- src/components/History.spec.js
+++ src/components/History.spec.js
@@ -1,13 +1,3 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import App from './App'
+it.todo('renders a History div with previous prompts')
 
-it('renders the Prompt component', () => {
-  render(<App />)
-  expect(screen.getByText(/prompt/i)).toBeVisible()
-})
-
-it('Text changes on button click -> the word "prompt" from the initial state is no longer displayed', () => {
-  render(<App />)
-  fireEvent.click(screen.getByText(/Spin!/i))
-  expect(screen.queryByText(/prompt/i)).toBeNull()
-})
+it.todo('the same prompt is not given twice')
