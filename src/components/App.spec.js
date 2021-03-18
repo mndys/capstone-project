04e6c6bbefeba8cd6ec2c95009c35d7a8352a500
Mandy Schwarze <mandy2301@me.com>
2021-03-18 15:46:47 +0100

    Structure tests; add Wheel test

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -2,33 +2,49 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 
-it('renders the App grid', () => {
-  render(<App />)
-  expect(screen.getByRole('banner').closest('div')).toHaveStyle('display: grid')
+describe('App', () => {
+  it('renders the App grid', () => {
+    render(<App />)
+    expect(screen.getByRole('banner').closest('div')).toHaveStyle(
+      'display: grid'
+    )
+  })
 })
 
-it('renders a Button with textContent "Spin!"', () => {
-  render(<App />)
-  expect(screen.getByRole('button', { name: 'Spin!' })).toBeVisible()
+describe('Buttons', () => {
+  it('renders a Button with textContent "Spin!"', () => {
+    render(<App />)
+    expect(screen.getByRole('button', { name: 'Spin!' })).toBeVisible()
+  })
 })
 
-it('writes to localStorage once on spin button click', () => {
-  jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem')
-  render(<App />)
-  userEvent.click(screen.getByRole('button', { name: /spin!/i }))
-  expect(window.localStorage.setItem).toHaveBeenCalledTimes(1)
+describe('Local Storage', () => {
+  it('writes to localStorage once on spin button click', () => {
+    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem')
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /spin/i }))
+    expect(window.localStorage.setItem).toHaveBeenCalledTimes(1)
+  })
+
+  it('writes to localStorage twice on reset button click', () => {
+    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem')
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /reset/i }))
+    expect(window.localStorage.setItem).toHaveBeenCalledTimes(2)
+  })
+
+  it('gets currentProperty and promptHistory from localStorage on reload', () => {
+    jest.spyOn(Object.getPrototypeOf(window.localStorage), 'getItem')
+    render(<App />)
+    expect(window.localStorage.getItem).toHaveBeenCalledTimes(2)
+  })
 })
 
-it('writes to localStorage twice on reset button click', () => {
-  jest.spyOn(Object.getPrototypeOf(window.localStorage), 'setItem')
-  render(<App />)
-  userEvent.click(screen.getByRole('button', { name: /reset/i }))
-  expect(window.localStorage.setItem).toHaveBeenCalledTimes(2)
-})
-
-it('gets currentProperty and promptHistory from localStorage on reload', () => {
-  jest.spyOn(window.localStorage.__proto__, 'getItem')
-  render(<App />)
-  window.location.reload()
-  expect(window.localStorage.getItem).toHaveBeenCalledTimes(2)
+describe('Wheel', () => {
+  it('starts spinning the wheel on spin button click', () => {
+    const INITIAL_PROMPT = 'Spin to receive your first prompt.'
+    render(<App />)
+    userEvent.click(screen.getByRole('button', { name: /spin/i }))
+    expect(screen.getByTestId('prompt').textContent).not.toBe(INITIAL_PROMPT)
+  })
 })
