6a3decd549b7380c0053da0be3a31ac0382bd1a7
Mandy Schwarze <mandy2301@me.com>
2021-03-18 11:49:01 +0100

    Add Wheel component story

---
+++ src/components/Wheel.stories.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Wheel } from 'react-custom-roulette'
+
+export default {
+  title: 'ui/Wheel',
+  component: Wheel,
+  argTypes: {},
+}
+
+const Template = args => (
+  <Wheel
+    {...args}
+    textColors={['#0e1516']}
+    fontSize="20"
+    outerBorderColor="#e4e4e4"
+    outerBorderWidth="10"
+    innerRadius="0"
+    innerBorderColor="#fff"
+    innerBorderWidth="60"
+    radiusLineColor="#fff"
+    radiusLineWidth="5"
+    // perpendicularText
+    textDistance="63"
+    mustStartSpinning
+  />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  prizeNumber: 0,
+  data: [
+    { option: 'change' },
+    { option: 'prize' },
+    { option: 'number' },
+    { option: 'to' },
+    { option: 'spin' },
+    { option: 'the' },
+    { option: 'wheel' },
+  ],
+  backgroundColors: [
+    '#f94144',
+    '#f3722c',
+    '#f8961e',
+    '#f9c74f',
+    '#90be6d',
+    '#43aa8b',
+    '#577590',
+  ],
+  /* textColors: ['#0e1516'],
+  fontSize: 12,
+  outerBorderColor: '#e4e4e4',
+  outerBorderWidth: 10,
+  innerRadius: 0,
+  innerBorderColor: '#fff',
+  innerBorderWidth: 60,
+  radiusLineColor: '#fff',
+  radiusLineWidth: 5,
+  // perpendicularText
+  textDistance: 63,
+  onStopSpinning: () => {
+    Default.args.mustStartSpinning = false
+  }, */
+}
