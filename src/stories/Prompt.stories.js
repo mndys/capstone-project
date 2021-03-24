7db62f6f46a23085c5b1bba3f39249446bb48a98
Mandy Schwarze <mandy2301@me.com>
2021-03-24 22:09:30 +0100

    Include color getter in stories

--- src/stories/Prompt.stories.js
+++ src/stories/Prompt.stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Color from '../components/Color'
 import Prompt from '../components/Prompt'
 
 export default {
@@ -7,9 +8,41 @@ export default {
   argTypes: {},
 }
 
-const Template = args => <Prompt {...args}>{args.text}</Prompt>
+const Template = args => (
+  <Prompt {...args}>
+    {args.text}
+    {args.colorObject ? (
+      <Color
+        colorObject={{
+          name: 'Japanese Carmine',
+          hex: '#9D2933',
+        }}
+        triggerPrompt="Cover Colour"
+      />
+    ) : (
+      ''
+    )}
+  </Prompt>
+)
 
 export const Default = Template.bind({})
 Default.args = {
   text: 'Go fish!',
+}
+
+export const ColorPrompt = Template.bind({})
+ColorPrompt.args = {
+  text: 'Cover Colour',
+  prompts: [
+    {
+      option: 'Cover Colour',
+      info:
+        'Read a book with this colour on the cover, spine, or in the title:',
+    },
+  ],
+  calculateCurrentPromptNumber: 0,
+  colorObject: {
+    name: 'Japanese Carmine',
+    hex: '#9D2933',
+  },
 }
