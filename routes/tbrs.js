23b7abdace38a8281561270e95cc0babb6af31ec
Mandy Schwarze <mandy2301@me.com>
2021-03-27 04:38:41 +0100

    take last changes back

--- routes/tbrs.js
+++ routes/tbrs.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const Tbr = require('../models/Tbr.js')
+const Tbr = require('../models/Tbr')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
