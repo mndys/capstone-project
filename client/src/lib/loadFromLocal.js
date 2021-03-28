944fb3b835bece3c63a2e4a7b2792b946a8c4d68
Mandy Schwarze <mandy2301@me.com>
2021-03-28 22:22:54 +0200

    Revert error message on loadFromLocal

--- client/src/lib/loadFromLocal.js
+++ client/src/lib/loadFromLocal.js
@@ -2,6 +2,6 @@ export default function loadFromLocal(key) {
   try {
     return JSON.parse(localStorage.getItem(key))
   } catch (error) {
-    console.log(`could not get item by key "${key}"`)
+    console.error(error)
   }
 }
