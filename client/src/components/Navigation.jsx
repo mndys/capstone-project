7d634b86e7c7d4df38cd75d6927dd53e888c6201
Mandy Schwarze <mandy2301@me.com>
2021-03-31 15:45:40 +0200

    Animate navigation

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -8,43 +8,70 @@ export default function Navigation() {
   return (
     <>
       <NavContainer className={active ? 'active' : ''}>
-        <NavLink className="link-2" to="/tbr">
+        <NavLink
+          className="link-2"
+          to="/tbr"
+          onClick={() => setActive(!active)}
+        >
           Books
         </NavLink>
-        <NavLink className="link-3" to="/monthly-tbr">
+        <NavLink
+          className="link-3"
+          to="/monthly-tbr"
+          onClick={() => setActive(!active)}
+        >
           Monthly TBR
         </NavLink>
-        <NavLink className="link-4" to="/add">
+        <NavLink
+          className="link-4"
+          to="/add"
+          onClick={() => setActive(!active)}
+        >
           Add book to TBR
         </NavLink>
-        <NavLink className="link-1" role="button" to="/">
+        <NavLink
+          className="link-1"
+          role="button"
+          to="/"
+          onClick={() => setActive(!active)}
+        >
           Wheel
         </NavLink>
       </NavContainer>
-      <Hamburger onClick={() => setActive(!active)} active={active} />
+      <Hamburger
+        onClick={() => setActive(!active)}
+        active={active}
+        onClick={() => setActive(!active)}
+      />
     </>
   )
 }
 
 const NavContainer = styled.nav`
   position: absolute;
+  bottom: -370px;
   right: 0;
-  bottom: -100vh;
-  text-align: right;
-  width: min-content;
-  transition: ease-in-out 0.5s;
-  background: linear-gradient(#f9f9f9dd 0%, #e5e5e5dd 100%);
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-end;
   align-content: flex-end;
-  padding: 10px 12px 50px 10px;
-  transition: 1s ease-in-out;
+  overflow: hidden;
+  width: 160px;
+  max-height: 0;
+  overflow: hidden;
+  text-align: right;
+  padding: 0;
+  background: linear-gradient(#f9f9f9dd 0%, #e5e5e5dd 100%);
   border-radius: 2px;
+  transition: all 750ms cubic-bezier(0.32, 1.25, 0.375, 1.15),
+    transform 0.3s ease-out 0.1s;
 
   &.active {
     bottom: 0;
-    transition: all 750ms cubic-bezier(0.32, 1.25, 0.375, 1.15);
+    max-height: 500px;
+    padding: 10px 12px 50px 10px;
+    transition: all 750ms cubic-bezier(0.32, 1.25, 0.375, 1.15),
+      transform 0.3s ease-out 0.1s;
   }
   a {
     display: block;
@@ -59,4 +86,3 @@ const NavContainer = styled.nav`
     border-right: 3px solid #093a40;
   }
 `
-const FlexWrapper = styled.div``
